abd966b201b6f0e9ba99b7dcf5630780dbe17660
HmlnSf <60872931+HmlnSf@users.noreply.github.com>
2022-12-17 20:50:01 +0200

    lab3.3

--- Regular expressions/code/task3/task3.php
+++ Regular expressions/code/task3/task3.php
@@ -1,85 +1,101 @@
-<?php
-if (!file_exists('./data')) {
-    mkdir('./data');
-}
-?>
-<h1>Добавить название категории</h1>
-<form action="task3.php" method="post">
-    <input type="text" name="category" placeholder="Название категории">
-    <input type="submit" value="Добавить">
-</form>
-<?php
-if (!empty($_POST['category'])) {
-    $category = $_POST['category'];
-    $path = "./data/" . $category;
-    if (!file_exists($path)) {
-        mkdir($path);
-    }
-}
+// Написать доску объявлений. Пользователь указывает свой
+// email, текст объявления, заголовок объявления (форма),
+// категория. Для хранения объявлений использовать файлы.
+// a) Создать несколько папок категорий.
 
-?>
-<h1>Добавить объявление</h1>
-<form action="task3.php" method="post">
-    <input type="text" name="title" placeholder="Заголовок объявления">
-    <textarea name="text" cols="30" rows="10" placeholder="Текст объявления"></textarea>
-    <input type="text" name="email" placeholder="Email">
-    <select name="category">
-        <?php
-        $dir = "./data/";
-        $files = scandir($dir);
-        foreach ($files as $file) {
-            if ($file != "." && $file != "..") {
-                echo "<option value='$file'>$file</option>";
-            }
-        }
-        ?>
-    </select>
-    <input type="submit" value="Добавить">
-</form>
 <?php
 
-if (!empty($_POST['category']) && !empty($_POST['title']) && !empty($_POST['text']) && !empty($_POST['email'])) {
-    $category = $_POST['category'];
-    $title = $_POST['title'];
-    $text = $_POST['text'];
-    $email = $_POST['email'];
-    $path = "./data/" . $category . "/" . $title . ".txt";
-    if (!file_exists($path)) {
-        $file = fopen($path, "w");
-        fwrite($file, $text . "\n" . $email);
-        fclose($file);
-    }
+const FILES_PATH = 'files/';
+
+function getFiles(array $directories): array
+{
+	$result = [];
+	foreach ($directories as $directory)
+	{
+		$files = array_diff(scandir(FILES_PATH  . $directory), ['.', '..']);
+		foreach ($files as $file)
+		{
+			$filePath =  FILES_PATH . $directory . '/' . $file;
+			$fileContent = file_get_contents($filePath);
+			$fileHeader = preg_replace('/.txt$/', '', $file);
+			$result[] = [
+				'header' => $fileHeader,
+				'content' => $fileContent,
+				'category' => $directory
+			];
+		}
+	}
+
+	return $result;
 }
+
+$categories = array_diff(scandir('./files'), ['.', '..']);
+$adverts = getFiles($categories);
+
 ?>
-<h1>Список объявлений</h1>
+
+// б) Необходимо чтобы на странице была форма с полями
+// (email, выбор категории(название выше созданных
+// папок), заголовок объявления и текст объявления,
+// кнопка Добавить).
+
+<form action="" method="post">
+	<p>Введите электронную почту<p>
+		<label>
+			<input type="email" name="email"/>
+		</label>
+	<p>Выберите категорию<p>
+		<label>
+			<select name="category">
+				<?php foreach ($categories as $category):?>
+					<option><?php echo $category?></option>
+				<?php endforeach;?>
+			</select>
+		</label>
+	<p>Введите заголовок объявления<p>
+		<label>
+			<input type="text" name="header"/>
+		</label>
+	<p>Введите текст объявления<p>
+		<label>
+			<textarea rows="6" cols="36" name="content"></textarea>
+		</label>
+		<input type="submit" value="Отправить">
+</form>
+
+// c) После формы список уже загруженных объявлений в
+// виде таблички.
 
 <table>
-    <tr>
-        <th>title</th>
-        <th>description</th>
-        <th>category</th>
-    </tr>
-    <?php
-    $files = scandir($dir);
-    foreach ($files as $file) {
-        if ($file != "." && $file != "..") {
-            $path = "./data/" . $file;
-            $files2 = scandir($path);
-            foreach ($files2 as $file2) {
-                if ($file2 != "." && $file2 != "..") {
-                    $path2 = "./data/" . $file . "/" . $file2;
-                    $file3 = fopen($path2, "r");
-                    $text = fgets($file3);
-                    $email = fgets($file3);
-                    fclose($file3);
-                    echo "<tr>";
-                    echo "<td>" . substr($file2, 0 , -4) . "</td>";
-                    echo "<td>" . $text . "</td>";
-                    echo "<td>" . $file . "</td>";
-                    echo "</tr>";
-                }
-            }
-        }
-    }
-    ?>
+	<tr>
+		<th>Категория</th>
+		<th>Заголовок</th>
+		<th>Текст объявления</th>
+	</tr>
+	<?php foreach ($adverts as $advert): ?>
+		<tr>
+			<th><?php echo $advert['category']?></th>
+			<th><?php echo $advert['header']?></th>
+			<th><?php echo $advert['content']?></th>
+		</tr>
+	<?php endforeach;?>
 </table>
+
+// d) После того, как пользователь нажал кнопку “добавить”,
+//необходимо создать новый текстовый файл
+//категория/заголовок_объявления.txt, содержимое
+//файла - Текст объявления.
+
+<?php
+if (
+	array_key_exists('email', $_POST)
+	&& array_key_exists('category', $_POST)
+	&& array_key_exists('header', $_POST)
+	&& array_key_exists('content', $_POST)
+)
+{
+	$filePath = FILES_PATH . $_POST['category'] . '/' . $_POST['header'] . '.txt';
+	$file = fopen($filePath, 'wb');
+	fwrite($file, $_POST['content']);
+}
+?>
