e4241f3e3c26fc60ff934951345bdcba6aa6b2d3
HmlnSf <60872931+HmlnSf@users.noreply.github.com>
2022-12-17 20:23:55 +0200

    lab3.2c

--- Regular expressions/code/task2/task2c.php
+++ Regular expressions/code/task2/task2c.php
@@ -1,22 +1,46 @@
-<?php
-session_start();
-if (!empty($_POST['name']) && !empty($_POST['age']) && !empty($_POST['salary']) && !empty($_POST['position'])) {
-    $_SESSION['userinfo'] = array(
-        'name' => $_POST['name'],
-        'age' => $_POST['age'],
-        'salary' => $_POST['salary'],
-        'position' => $_POST['position']
-    );
-}
-?>
-<form action="task2c.php" method="post">
-    <input type="text" name="name" placeholder="Имя">
-    <input type="text" name="age" placeholder="Возраст">
-    <input type="text" name="salary" placeholder="Зарплата">
-    <input type="text" name="position" placeholder="Должность">
-    <input type="submit" value="Отправить">
+// c) На одной странице с помощью формы спросите у
+// пользователя имя, возраст, зарплату и еще что-нибудь.
+// Запишите эти данные в одну переменную сессии в виде
+// массива. При заходе на другую страницу переберите
+// сохраненные данные циклом и выведите каждый элемент
+// массива в своем теге li тега ul.
+
+<?php session_start() ?>
+
+<form action="" method="post">
+	<p>Введите ваше имя<p>
+		<label>
+			<input type="text" name="name"/>
+		</label>
+	<p>Введите ваш возраст<p>
+		<label>
+			<input type="text" name="age"/>
+		</label>
+	<p>Введите вашу зарплату<p>
+		<label>
+			<input type="text" name="salary"/>
+		</label>
+	<p>Кем вы хотели стать в детстве?<p>
+		<label>
+			<input type="text" name="question"/>
+		</label>
+		<input type="submit" value="Отправить">
 </form>
 
-<form action="task2c_show.php" method="post">
-    <input type="submit" value="Показать информацию">
-</form>
+<a href="task2c_show.php">Перейти к просмотру результатов</a>
+
+<?php
+if (
+	array_key_exists('name', $_POST)
+	&& array_key_exists('age', $_POST)
+	&& array_key_exists('salary', $_POST)
+	&& array_key_exists('question', $_POST)
+)
+{
+	$_SESSION['result'] = [
+		'name' => $_POST['name'],
+		'age' => $_POST['age'],
+		'salary' => $_POST['salary'],
+		'question' => $_POST['question']
+	];
+}
