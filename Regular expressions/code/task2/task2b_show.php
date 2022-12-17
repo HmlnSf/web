26d8f6e95b12ab3ebff789ee1a0eec971241ba6e
HmlnSf <60872931+HmlnSf@users.noreply.github.com>
2022-12-17 20:11:04 +0200

    lab3.2b

--- Regular expressions/code/task2/task2b_show.php
+++ Regular expressions/code/task2/task2b_show.php
@@ -1,10 +1,5 @@
-<?php
-session_start();
-if (!empty($_SESSION['name']) && !empty($_SESSION['surname']) && !empty($_SESSION['age'])) {
-    echo nl2br("\n" . "Имя: " . $_SESSION['name'] . "\n");
-    echo nl2br("\n" . "Фамилия: " . $_SESSION['surname'] . "\n");
-    echo nl2br("\n" . "Возраст: " . $_SESSION['age'] . "\n");
-}
-else {
-    echo nl2br("\n" . "Нет данных" . "\n");
-}
+<?php session_start() ?>
+
+<p>Ваша фамилия: <?php echo $_SESSION['surname'] ?></p>
+<p>Ваше имя: <?php echo $_SESSION['name'] ?></p>
+<p>Ваш возраст: <?php echo $_SESSION['age'] ?></p>
