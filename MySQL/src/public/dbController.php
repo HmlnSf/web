a31abfe203aad647c1424f5c4f73eedadc8c93fa
HmlnSf <60872931+HmlnSf@users.noreply.github.com>
2022-12-18 21:41:26 +0200

    lab5

--- MySQL/src/public/dbController.php
+++ MySQL/src/public/dbController.php
@@ -1,45 +1,113 @@
 <?php
 
-class dbController
+function connectToDb(array $config): mysqli
 {
-    private mysqli $db;
+	return new mysqli(
+		$config['hostname'],
+		$config['username'],
+		$config['password'],
+		$config['database']
+	);
+}
 
-    public function connect()
-    {
-        try {
-            $this->db = new mysqli('db', 'root', 'test123', 'web');
-        } catch (Exception $e) {
-            echo 'Error connecting to database';
-        }
-    }
+function checkParams(array $params): bool
+{
+	return array_key_exists('email', $params)
+		&& array_key_exists('category', $params)
+		&& array_key_exists('title', $params)
+		&& array_key_exists('description', $params)
+	;
+}
 
-    public function createTable()
-    {
-        $sql = "CREATE TABLE IF NOT EXISTS adverts(
-            id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            titles VARCHAR(255) NOT NULL,
-            name VARCHAR(255) NOT NULL,
-            description VARCHAR(255) NOT NULL,
-            email VARCHAR(255) NOT NULL
-        )";
-        $this->db->query($sql);
-    }
+function createAd(mysqli $mysqli, array $params)
+{
+	$queryStr = "INSERT INTO ad (EMAIL, TITLE, DESCRIPTION, CATEGORY)
+		VALUES ('{$params['email']}', '{$params['title']}', '{$params['description']}', '{$params['category']}')
+	";
 
-    public function addAdvert($titles, $name, $desc, $email)
-    {
-        $sql = "INSERT INTO adverts(titles, name, description, email) VALUES(
-            '{$titles}',
-            '{$name}',
-            '{$desc}',
-            '{$email}'
-        )";
-        $this->db->query($sql);
-    }
+	$mysqli->query($queryStr);
+}
 
-    public function getAdverts()
-    {
-        $sql = "SELECT * FROM adverts";
-        $result = $this->db->query($sql);
-        return $result;
-    }
+function getAdList(mysqli $mysqli)
+{
+	$result = [];
+	$queryStr = "SELECT * FROM ad ORDER BY DESCRIPTION DESC";
+
+	$queryResult = $mysqli->query($queryStr);
+	if ($queryResult)
+	{
+		while ($row = $queryResult->fetch_assoc())
+		{
+			$result[] = $row;
+		}
+
+		$queryResult->close();
+	}
+
+	return $result;
+}
+
+function renderForm()
+{
+	echo '<form action="" method="post">
+		<p>Введите электронную почту<p>
+			<label>
+				<input type="email" name="email"/>
+			</label>
+		<p>Выберите категорию<p>
+			<label>
+				<select name="category">'
+					. getCategoryOptions() .
+				'</select>
+			</label>
+		<p>Введите заголовок объявления<p>
+			<label>
+				<input type="text" name="title"/>
+			</label>
+		<p>Введите текст объявления<p>
+			<label>
+				<textarea rows="6" cols="36" name="description"></textarea>
+			</label>
+		<input type="submit" value="Отправить">
+	</form>';
+}
+
+function getCategoryOptions(): string
+{
+	$result = '';
+	$categories = array_diff(scandir('../lab3/files'), ['.', '..']);
+
+	foreach ($categories as $category)
+	{
+		$result .= "<option>$category</option>";
+	}
+
+	return $result;
+}
+
+function renderAdsContent(array $selectedAds)
+{
+	$adsContent = '';
+	foreach ($selectedAds as $ad)
+	{
+		$fields = '';
+		foreach ($ad as $field)
+		{
+			$fields .= "<td>" . htmlspecialchars($field) . "</td>";
+		}
+
+		$adsContent .= "<tr>" . $fields . "</tr>";
+	}
+
+	echo '<table>
+	<tr>
+		<th>ID</th>
+		<th>Электронная почта</th>
+		<th>Заголовок</th>
+		<th>Текст объявления</th>
+		<th>Категория</th>
+		<th>Дата создания</th>
+	</tr>
+	' . $adsContent . '
+	</table>';
 }
